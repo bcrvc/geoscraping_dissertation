a5e51cd137f0fd4c384cbe6b92738db3f3de84d4
Goran Becirevic <goran@middlemachine.com>
2014-07-20 16:37:13 +0100

    Create flickr_imgdata.py
    Grabbing Flickr img metadata and printing it in an xml or json format.

---
+++ flickr_imgdata.py
@@ -0,0 +1,30 @@
+#################################################################
+# this code can be used for getting the image metadata          #
+# from FLickr via their API - image search can be modified      #
+# following this API method:                                    #
+# https://www.flickr.com/services/api/flickr.photos.search.html #
+#################################################################
+
+# prints xml data format: 
+
+import flickrapi
+import xml
+
+api_key='********************************'
+api_secret ='****************'
+             
+flickr = flickrapi.FlickrAPI(api_key,secret=api_secret)
+r = flickr.photos_search(tags='*******', has_geo="1", extras='geo, license, date_upload, path_alias, url_n', per_page='250', page='**')
+xml.etree.ElementTree.dump(r)
+
+## or in json data format: 
+
+# import flickrapi
+# import json
+
+# api_key='********************************'
+# api_secret ='****************'
+             
+# flickr = flickrapi.FlickrAPI(api_key,secret=api_secret)
+# r = flickr.photos_search(tags='*******', has_geo="1", extras='geo, license, date_upload, path_alias, url_n', per_page='250', page='**')
+# print r
