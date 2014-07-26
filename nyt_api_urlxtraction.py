573d1483ed4b3482025054fcbce4fa2bb13b4c1c
Goran Becirevic <goran@middlemachine.com>
2014-07-26 13:10:03 +0100

    Create nyt_api_urlxtraction.py

---
+++ nyt_api_urlxtraction.py
@@ -0,0 +1,24 @@
+'''
+script for getting the article data out of NYT API ...
+extract the articles URLs and store them into a .txt file
+'''
+
+import urllib
+import json
+
+# get the dataset via NYT API 
+# guide to constructing a search query: http://developer.nytimes.com/docs/read/article_search_api_v2#h2-queries
+htmltext = urllib.urlopen('*******http://API-call********')
+data = json.load(htmltext)
+
+# open a .txt file 
+myfile = open('urls.txt', 'w') 
+
+# extract all URLs and store them into a .txt file 
+for doc in data['response']['docs']:
+	  # if print/write doc = whole data file 
+    url = doc['web_url']
+    print url
+    myfile.write('%s\n' % url)
+
+myfile.close()
